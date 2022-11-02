8c1e96f247e73b4cbdcd596dad5bb5016c42a96f
realRunlo <gteixeira.pereira@gmail.com>
2022-11-02 17:27:20 +0000

    api to db, process games info data for display

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from accounts.models import User,Session
-from game.models import load_ucras,Game,Odd
+from game.models import load_ucras,Game,Odd,Odd_type
 import requests
 
 # Create your views here.
@@ -8,18 +8,28 @@ def home(request):
     #load ucras api to database
     load_ucras('http://ucras.di.uminho.pt/v1/games/')
     cookie = request.COOKIES.get("session")
-
     # get all games
     games = Game.objects.all().values()
-    odds = Odd.objects.all().values()
-    print(odds)
-    
-    context = {
-                    "logged" : False,
-                    "games" : games,
-                    "odds" : odds
-                }
-   # try:
+    print(games)
+    main_listing = []
+    # Group each game with the odds in a dictionary
+    for g in games:
+        odds = Odd.objects.filter(game_id=g['game_id'])
+        game_dict = {}
+        game_dict["game"] = g
+        for odd_obj in odds:
+            type =  getattr(odd_obj, "odd_type")
+            odd = getattr(odd_obj, "odd")
+            if type.str() == "home":
+                game_dict["home_odd"] = odd
+            elif type.str() == "away":
+                game_dict["away_odd"] = odd
+            elif type.str() == "draw":
+                game_dict["draw_odd"] = odd
+        main_listing.append(game_dict)
+
+
+    print(main_listing)
     if cookie:
         session = Session.objects.get(session_id=cookie)
         context = {
@@ -28,14 +38,12 @@ def home(request):
                 "id" : session.user_in_session.userID,
                 "fname" : session.user_in_session.first_name,
                 "balance" : session.user_in_session.balance,
-                "games" : games,
-                "odds" : odds
+                "games_info" : main_listing,
         }
-    response = render(request, 'index.html',context)
-    ''' #tratar de quando cookie existe, mas a sessao nao
-    except Exception:
-        response = render(request, 'index.html',context)
-        if cookie:   
-            response.delete_cookie('session') 
-'''
-    return response
+    else:
+        context = {
+                "logged" : False,
+                "games_info" : main_listing
+            }
+
+    return render(request, 'index.html',context)
