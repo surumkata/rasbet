df576773eee4ff4a1f01397604d08837f89ec32b
Tiago Silva <a93277@alunos.uminho.pt>
2022-11-03 15:25:05 +0000

    remove do boletim jogos repetidos

--- main/static/bettingslip.js
+++ main/static/bettingslip.js
@@ -1,18 +1,34 @@
 
 function slip(elem){
-  console.log(elem.name);
+    elem.checked = true
+    console.log(elem.name);
+    rm = document.getElementById(elem.name)
+    if(rm != null && typeof(rm) != 'undefined'){
+      rm.remove()
+    }
 
-  let slip = document.querySelector('.slip');
+    let slip = document.querySelector('.slip');
 
-  let html = slip.outerHTML;
+    // Get data atributes from tag
+    var home = elem.getAttribute("data-home")
+    var away = elem.getAttribute("data-away")
+    var bet = elem.getAttribute("data-bet")
 
-  // Get data atributes from tag
-  var home = elem.getAttribute("data-home")
-  var away = elem.getAttribute("data-away")
-  var bet = elem.getAttribute("data-bet")
+    //Add game to slip in the html
+    slip.outerHTML = '<div id='+elem.name+'><input type="hidden" name='+ elem.name + '>' + home + '-' + away + '<br>Resultado(Tempo Regulamentar): ' + bet +'</input> <br><br></div>' + slip.outerHTML;
 
-  //Add game to slip in the html
-  slip.outerHTML = '<input type="hidden" id='+ elem.name + 'name='+ elem.name + '>' + home + '-' + away + '<br>Resultado(Tempo Regulamentar): ' + bet +'</input> <br><br>' + slip.outerHTML;
+}
 
-
+function uncheck(elem){
+  if(elem.checked){
+    console.log("unchecked")
+    elem.checked = false
+    rm = document.getElementById(elem.name)
+    if(rm != null && typeof(rm) != 'undefined'){
+      rm.remove()
+    }
+  }else{
+    console.log("checked")
+    elem.checked = true
+  }
 }
