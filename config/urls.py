6271b44f9405edf4390af55812f413943b92f3d5
Tiago Silva <a93277@alunos.uminho.pt>
2022-11-03 09:46:54 +0000

    favicon

--- config/urls.py
+++ config/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.templatetags.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/',admin.site.urls),
     path('',include('main.urls')),
     path('accounts/', include('accounts.urls')),
     path('game/', include('game.urls')),
-
+    path('favicon.ico', RedirectView.as_view(url=static('favicon.ico'))),
 ]
+
