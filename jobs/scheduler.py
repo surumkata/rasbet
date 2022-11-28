d821230b4359eb994aea7e356370e9b21cc9f98a
Gonçalo Pereira <gteixeira.pereira@gmail.com>
2022-11-28 10:19:23 +0000

    schedular intervalTrigger

--- jobs/scheduler.py
+++ jobs/scheduler.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
 from .jobs import load_ucras
 
 
 def start():
     sheduler = BackgroundScheduler()
-    sheduler.add_job(load_ucras,'interval',seconds = 20,max_instances = 1)
+    sheduler.add_job(load_ucras,IntervalTrigger(seconds=20),max_instances = 1)
     sheduler.start()
