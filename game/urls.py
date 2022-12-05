02fb84b7fe5b2baa3f51b9f61cc64abd33003728
brazafonso <61985783+brazafonso@users.noreply.github.com>
2022-12-05 11:43:33 +0000

    index pt

--- game/urls.py
+++ game/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('filter/',views.filter,{}),
     path('filter_specialist/',views.filter_specialist,{}),
-    path('change_games_state/',views.change_games_state,{}),
     path('specialist_update_games/',views.specialist_update_games,{}),
 ]
